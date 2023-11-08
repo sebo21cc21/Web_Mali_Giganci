39ef74b5ccbe50a3c453dc6fd5f7e2062c45aeab
Sebastian Bednarski <sebo21cc21@gmail.com>
2023-11-08 01:18:41 +0100

    add working oauth 2.0 :100:

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule],
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent {
+  template: `
+    <mat-toolbar color="secondary">
+      <span>MaliGiganci</span>
 
+      <button mat-flat-button (click)="logOut()">Wyloguj się</button>
+    </mat-toolbar>
+    
+  `,
+  styles: [
+    `
+      mat-toolbar {
+        display: flex;
+        justify-content: space-between;
+      }
+    `,
+  ],
+})
+export default class HomeComponent {
+  private _router = inject(Router);
+
+  private authservice = inject(AuthService);
+
+  async logOut(): Promise<void> {
+    try {
+      await this.authservice.logOut();
+      this._router.navigateByUrl('/auth/log-in');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,11 @@
-// Your web app's Firebase configuration
-export const environment ={
-    production: false,
-
-    firebaseConfig: {
-        apiKey: "AIzaSyCSK0EqoEzi-bnnkl1ECKJsvmSH7Z6ald4",
-        authDomain: "newpro-d4342.firebaseapp.com",
-        projectId: "newpro-d4342",
-        storageBucket: "newpro-d4342.appspot.com",
-        messagingSenderId: "524997788627",
-        appId: "1:524997788627:web:5f8f179e84e15f16bde526",
-        measurementId: "G-XJ2717PRPH"
-    }
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDbKT3HXdXmYYdBHU9D5uel7_B9VGqlLhU",
+    authDomain: "zpodb-13865.firebaseapp.com",
+    databaseURL: "https://zpodb-13865-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "zpodb-13865",
+    storageBucket: "zpodb-13865.appspot.com",
+    messagingSenderId: "361827856884",
+    appId: "1:361827856884:web:1b2cfccd94ce553c6d09a1"
+  },
 };
